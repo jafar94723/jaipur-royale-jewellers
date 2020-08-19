588628aee8aab13f333994d2be8e03c09a0b920f
mak947 <mak94723@gmail.com>
2020-08-19 12:47:46 +0530

    removed node modules

---
+++ notes/1. css in js.js
@@ -0,0 +1,14 @@
+/*
+CSS files are all global
+
+so reusing classNames
+in other components
+will cause issues.
+
+solutions:-
+
+1. specific naming : brings a lot overhead
+2. using style attribute : can't use all selectors like :hover
+3. use libraries : styled-components, radium, aphrodite
+
+*/
