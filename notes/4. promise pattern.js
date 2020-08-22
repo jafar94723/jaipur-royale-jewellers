381d7c740a9677bb774f6c84b70231911347ce4b
mak947 <mak94723@gmail.com>
2020-08-22 21:21:24 +0530

    async redux

---
+++ notes/4. promise pattern.js
@@ -0,0 +1,15 @@
+/*
+
+
+collectionRef.onSnapshot(snapshot => {})
+collectionRef.get().then(snapshot => {})
+
+difference:
+onSnapshot gives a live update stream style
+
+promise just runs on mount
+
+
+
+
+*/
---
+++ notes/5. when to use async redux.js
@@ -0,0 +1,8 @@
+/*
+
+when there's a chance 
+that fetching needs to happen
+in multiple components such as shop page
+and collection page.
+
+*/
