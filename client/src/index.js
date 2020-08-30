d33c76b040f81091422fd8eeef2317727092821a
mak947 <mak94723@gmail.com>
2020-08-30 20:44:56 +0530

    made app PWA and added cart sagas

--- client/src/index.js
+++ client/src/index.js
@@ -3,21 +3,20 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-//Redux
 import { Provider } from "react-redux";
-import {store,persistor} from './redux/store';
-import {PersistGate} from 'redux-persist/integration/react';
+import { store, persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
   <React.StrictMode>
-    {/* Provider is parent of everything in application
-    so that everything has access to store object */}
     <Provider store={store}>
       <BrowserRouter>
-      <PersistGate persistor={persistor}>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
         </PersistGate>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
+serviceWorker.register();
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-
+const enforce = require('express-sslify');
+const compression = require('compression');
 if(process.env.NODE_ENV !== 'production'){
     require('dotenv').config();
 }
@@ -13,9 +14,12 @@ const app = express();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(cors());
+app.use(compression);
 
 
 if(process.env.NODE_ENV === 'production'){
+    app.use(compression);
+    app.use(enforce.HTTPS({ trustProtoHeader: true }));
     app.use(express.static(path.join(__dirname,'client/build')));
     app.get('*', function(req,res){
         res.sendFile(path.join(__dirname,'client/build','index.html'));
@@ -29,6 +33,10 @@ app.listen(port, error=>{
     console.log(`Server running on port : ${port}`)
 });
 
+
+app.get('/service-worker.js', (req,res) => {
+    res.sendFile(path.resolve(__dirname,'..','build','service-worker.js'));
+})
 
 app.post("/payment", (req,res) => {
     const body = {
