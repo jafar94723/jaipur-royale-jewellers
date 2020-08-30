23d797b38e46ee8dabe57e9bfd31c01ea62bc7ea
mak947 <mak94723@gmail.com>
2020-08-30 23:04:52 +0530

    added media queries for mobile support

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Fragment,useEffect } from "react";
-import "./App.css";
+import "./App.scss";
 import HomePage from "./pages/homepage/homepage.container";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
